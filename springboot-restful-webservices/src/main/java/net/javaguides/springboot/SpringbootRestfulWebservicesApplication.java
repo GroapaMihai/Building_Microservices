7cd87065fec296ff2601408032d234d3d3627db6
Mihai Groapa <gmihai9549@gmail.com>
2023-05-16 16:42:10 +0300

    Rest API documentation using Swagger

--- springboot-restful-webservices/src/main/java/net/javaguides/springboot/SpringbootRestfulWebservicesApplication.java
+++ springboot-restful-webservices/src/main/java/net/javaguides/springboot/SpringbootRestfulWebservicesApplication.java
@@ -1,11 +1,25 @@
 package net.javaguides.springboot;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Contact;
+import io.swagger.v3.oas.annotations.info.Info;
 import org.modelmapper.ModelMapper;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
+@OpenAPIDefinition(
+	info = @Info(
+		title = "Spring Boot Rest Api Documentation",
+		description = "Spring Boot Rest Api Documentation",
+		version = "V1.0",
+		contact = @Contact(
+			name = "Mihai Groapa",
+			email = "groapa_mihai95@yahoo.com"
+		)
+	)
+)
 public class SpringbootRestfulWebservicesApplication {
 
 	@Bean
--- springboot-restful-webservices/src/main/java/net/javaguides/springboot/controller/UserController.java
+++ springboot-restful-webservices/src/main/java/net/javaguides/springboot/controller/UserController.java
@@ -1,5 +1,9 @@
 package net.javaguides.springboot.controller;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
 import lombok.AllArgsConstructor;
 import net.javaguides.springboot.dto.UserDto;
@@ -10,6 +14,10 @@ import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
+@Tag(
+        name = "CRUD REST APIs for User Resource",
+        description = "Create/Get/Update/Delete user, Get all users"
+)
 @RestController
 @AllArgsConstructor
 @RequestMapping("api/users")
@@ -18,8 +26,16 @@ public class UserController {
     private UserService userService;
 
     // build create User REST API
+    @Operation(
+        summary = "Create User REST API",
+        description = "Endpoint used to save a user in the database"
+    )
+    @ApiResponse(
+        responseCode = "201",
+        description = "HTTP Status 201 CREATED"
+    )
     @PostMapping
-    public ResponseEntity<UserDto> createUser(@Valid @RequestBody UserDto userDto){
+    public ResponseEntity<UserDto> createUser(@Valid @RequestBody UserDto userDto) {
         UserDto savedUser = userService.createUser(userDto);
 
         return new ResponseEntity<>(savedUser, HttpStatus.CREATED);
@@ -27,8 +43,18 @@ public class UserController {
 
     // build get user by id REST API
     // http://localhost:8080/api/users/1
+    @Operation(
+        summary = "Get User by id REST API",
+        description = "Endpoint used to retrieve a user by id from the database"
+    )
+    @ApiResponse(
+        responseCode = "200",
+        description = "HTTP Status 200 SUCCESS"
+    )
     @GetMapping("{id}")
-    public ResponseEntity<UserDto> getUserById(@PathVariable("id") Long userId){
+    public ResponseEntity<UserDto> getUserById(
+        @Parameter(description = "Id that uniquely identifies a user") @PathVariable("id") Long userId
+    ) {
         UserDto userDto = userService.getUserById(userId);
 
         return new ResponseEntity<>(userDto, HttpStatus.OK);
@@ -36,18 +62,36 @@ public class UserController {
 
     // Build Get All Users REST API
     // http://localhost:8080/api/users
+    @Operation(
+        summary = "Get All Users REST API",
+        description = "Endpoint used to retrieve all users from the database"
+    )
+    @ApiResponse(
+        responseCode = "200",
+        description = "HTTP Status 200 SUCCESS"
+    )
     @GetMapping
-    public ResponseEntity<List<UserDto>> getAllUsers(){
+    public ResponseEntity<List<UserDto>> getAllUsers() {
         List<UserDto> userDtos = userService.getAllUsers();
 
         return new ResponseEntity<>(userDtos, HttpStatus.OK);
     }
 
     // Build Update User REST API
+    @Operation(
+        summary = "Update User by id REST API",
+        description = "Endpoint used to update a user by id and save it in the database"
+    )
+    @ApiResponse(
+        responseCode = "200",
+        description = "HTTP Status 200 SUCCESS"
+    )
     @PutMapping("{id}")
     // http://localhost:8080/api/users/1
-    public ResponseEntity<UserDto> updateUser(@PathVariable("id") Long userId,
-                                              @Valid @RequestBody UserDto userDto){
+    public ResponseEntity<UserDto> updateUser(
+        @Parameter(description = "Id that uniquely identifies a user") @PathVariable("id") Long userId,
+        @Valid @RequestBody UserDto userDto
+    ) {
         userDto.setId(userId);
         UserDto updatedUser = userService.updateUser(userDto);
 
@@ -55,8 +99,18 @@ public class UserController {
     }
 
     // Build Delete User REST API
+    @Operation(
+        summary = "Delete User by id REST API",
+        description = "Endpoint used to delete a user by id in the database"
+    )
+    @ApiResponse(
+        responseCode = "200",
+        description = "HTTP Status 200 SUCCESS"
+    )
     @DeleteMapping("{id}")
-    public ResponseEntity<String> deleteUser(@PathVariable("id") Long userId){
+    public ResponseEntity<String> deleteUser(
+        @Parameter(description = "Id that uniquely identifies a user") @PathVariable("id") Long userId
+    ) {
         userService.deleteUser(userId);
         return new ResponseEntity<>("User successfully deleted!", HttpStatus.OK);
     }
