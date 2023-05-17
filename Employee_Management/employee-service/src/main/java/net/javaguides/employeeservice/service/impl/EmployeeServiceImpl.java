a171626ef7be8e4e67edb5f9957eadb07c9d77ed
Mihai Groapa <gmihai9549@gmail.com>
2023-05-17 14:39:55 +0300

    Added Exception Handling (GlobalExceptionHandler using @ControllerAdvice, @ExceptionHandler(ResourceNotFoundException.class))

--- Employee_Management/employee-service/src/main/java/net/javaguides/employeeservice/service/impl/EmployeeServiceImpl.java
+++ Employee_Management/employee-service/src/main/java/net/javaguides/employeeservice/service/impl/EmployeeServiceImpl.java
@@ -3,6 +3,7 @@ package net.javaguides.employeeservice.service.impl;
 import lombok.AllArgsConstructor;
 import net.javaguides.employeeservice.dto.EmployeeDto;
 import net.javaguides.employeeservice.entity.Employee;
+import net.javaguides.employeeservice.exception.ResourceNotFoundException;
 import net.javaguides.employeeservice.mapper.EmployeeMapper;
 import net.javaguides.employeeservice.repository.EmployeeRepository;
 import net.javaguides.employeeservice.service.EmployeeService;
@@ -25,7 +26,9 @@ public class EmployeeServiceImpl implements EmployeeService {
 
     @Override
     public EmployeeDto getEmployeeById(Long id) {
-        Employee employee = employeeRepository.findById(id).get();
+        Employee employee = employeeRepository.findById(id).orElseThrow(
+            () -> new ResourceNotFoundException("Employee", "id", id)
+        );
 
         return EmployeeMapper.INSTANCE.mapToEmployeeDto(employee);
     }
