085d4db43bab8f0ea04412e43acfd57b1c50c994
Mihai Groapa <gmihai9549@gmail.com>
2023-06-07 19:50:41 +0300

    Upgraded Spring Boot to version 3; Replaced Spring cloud Sleuth with Micrometer for distributed tracing (the Sleuth project is being moved into the Micrometer Tracing project in Spring Boot 3);

--- Employee_Management/employee-service/src/main/java/net/javaguides/employeeservice/EmployeeServiceApplication.java
+++ Employee_Management/employee-service/src/main/java/net/javaguides/employeeservice/EmployeeServiceApplication.java
@@ -2,7 +2,6 @@ package net.javaguides.employeeservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.client.RestTemplate;
@@ -10,7 +9,6 @@ import org.springframework.web.reactive.function.client.WebClient;
 
 @SpringBootApplication
 @EnableFeignClients
-@EnableEurekaClient
 public class EmployeeServiceApplication {
 
 	@Bean
